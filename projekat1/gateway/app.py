a57526b7d490810ff7dd21beaee4f81dfd48cdb1
asovi-afk <asovi12@gmail.com>
2022-12-05 00:44:51 +0100

    Fali  OpenAPI

--- projekat1/gateway/app.py
+++ projekat1/gateway/app.py
@@ -1,68 +1,141 @@
 import json
 from urllib import request
 import requests
-from flask import Flask
+import argparse
+from flask import Flask, request, Response
+from flask_mqtt import Mqtt
+
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
+
+from flasgger import Swagger, LazyString, LazyJSONEncoder, swag_from
+
 app = Flask(__name__)
 
+app.json_encoder = LazyJSONEncoder
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--project", dest = "project", default = 2, help = "Project 2 uses MQTT")
+parser.add_argument("-ua", "--use-api", dest = "use_api", default = False, help = "Using external API")
+args = parser.parse_args()
+
+swagger_template = dict(
+    info = {
+        'title': LazyString(lambda: 'Projekat 1  GATEWAY API'),
+        'version': LazyString(lambda: '1.0.0'),
+        'description': LazyString(lambda: 'Description...')
+    },
+    host = LazyString(lambda: request.host)
+)
+
+swagger = Swagger(app, template=swagger_template)
+
+app.config['MQTT_BROKER_URL'] = 'mosquitto'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_CLIENT_ID'] = 'gateway'
+app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
+
+topic = "analytics/requests/signal"
+#topic = "test"
+
+mqtt_client = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print('Connected to MQTT')
+    else :
+        print('Bad MQTT connection. Code: ', rc)
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'], data['qos'])
+
 url = "https://mdblist.p.rapidapi.com/"
-querystring = {"s":"jaws"}
 
 headers = {
     "X-RapidAPI-Key": "d1bc709fe3msh074561e6633c519p1f14efjsn5eb5c2d330cf",
     "X-RapidAPI-Host":"mdblist.p.rapidapi.com"
 }
 
+apiUrl = "http://api:3000/movie/"
+traktTvUrl = "https://trakt.tv/movies/"
+imdbUrl = "https://www.imdb.com/title/"
 
-# # ne bih da radimo sa serijama, dovoljni su filmovi
-# # routes
-# movie/alltitles
-# # response kako želiš
+def publis_title(title):
+    publish_result = mqtt_client.publish(topic, "{\"title\": \""+title+"\"}")
 
-# movie/suggestions
-# r = {
-#     "max-duration": 128, # in minutes
-#     "genere": ["Dramas", "Comedies"], # mozda ID za zanrove?
-#     "release-year": 1998, # from this year
-#     "director": ["Andy Devonshire", "Theodore Melfi"], # atleast one of listed
-#     "cast": ["Kofi Ghanaba", "Oyafunmike Ogunlano"] #atleast one of listed
-# }
-# # response -> [ {sve o filmu 1}, {sve o filmu 2}] sa ID-jem filma
+@swag_from("./swagger_routes/get_movie.yml")
+@app.route('/', methods = ['GET'])
+def get():
+    apiResponse = requests.request('GET', apiUrl, params=request.args.to_dict())
+    if apiResponse.status_code == 200:
+        movie = apiResponse.json()
+        print(movie)
+        if args.project == 2:
+            publis_title(movie["title"])
 
-# movie/delete
-# r ={
-#     "id": "movie id"
-# }
+        if args.use_api:
+            mdbResponse = requests.request('GET', url, headers=headers, params = {"s": movie["title"]}) 
+            if mdbResponse.status_code == 200:
+                mdb = mdbResponse.json()
+                if mdb["response"] == True and mdb["total"] > 0:
+                    for item in mdb["search"]:
+                        if item["type"] == "movie" and item["title"] == movie["title"]:
+                            movie.update({"score_average": item["score_average"]})
+                            if item["imdbid"] != None:
+                                movie.update({"imdb":imdbUrl+str(item["imdbid"])})
+                            if item["traktid"] != None:
+                                movie.update({"trakt":traktTvUrl+str(item["traktid"])})
+                            break
+        else:
+            movie.update({"score_average": None})
+            movie.update({"imdb": None})
+            movie.update({"trakt": None})
 
-# movie/create 
-# r = {
-#     "title": "ttttt",
-#     "director": ["dir 1", "dir 2"],
-#     "cast": ["ac 1", "ac 2"], 
-#     "country": "Serbia",
-#     #"date-added": "asatat" da se ne zanosimo sa formatom, izostavio bih ovo polje
-#     "release-year": 2022,
-#     "rating": "PG-13", # mozemo da izostavimo
-#     "genere": ["Comedies", "Dramas"],
-#     "description": "BIG BIG BIG string"
-# }
+        return movie, apiResponse.status_code
+    else :
+        return Response(status = apiResponse.status_code) 
 
-# movie/update
-# r = {
-#     "id": "s21",
-#     # svi parametri sa kojim želiš da radiš
-#     # ako je parametar null onda se ne menja
-#     # ako parametar nije null onda se postavlja
-# }
+#@swag_from("./swagger_routes/create_movie.yml")
+@app.route('/create', methods = ['POST'])
+def create():
+    data = request.get_json()
+    apiResponse = requests.request("POST", apiUrl+"create", json=data)
+    if apiResponse.status_code == 201:
+        return apiResponse.json(), apiResponse.status_code
+    else :
+        return Response(status = apiResponse.status_code) 
+        
+#@swag_from("swagger_routes/update_movie.yml")
+@app.route('/update', methods = ['PUT'])
+def update():
+    data = request.get_json()
+    apiResponse = requests.request("PUT", apiUrl+"update", json=data)
+    return Response(status = apiResponse.status_code) 
+
+#@swag_from("swagger_routes/delete_movie.yml")
+@app.route('/delete', methods = ['DELETE'])
+def delete():
+    data = request.get_json()
+    apiResponse = requests.request("DELETE", apiUrl+"update", json=data)
+    return Response(status = apiResponse.status_code) 
+
+#@swag_from("swagger_routes/suggestions_movie.yml")
+@app.route('/suggestions', methods = ['POST'])
+def suggestions():
+    data = request.get_json()
+    print(data)
+    apiResponse = requests.post(apiUrl+"suggestions", json=data)
+    print(apiResponse.status_code)
+    
+    return apiResponse.json() if apiResponse.status_code == 200 else Response(status=apiResponse.status_code)
 
 
-@app.route('/test/')
-def test():
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response.json()
-
-
-@app.route('/')
-def index():
-    return json.dumps({'name': 'alice',
-                       'email': 'alice@outlook.com'})
-app.run()
+# app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=False)
---
+++ projekat1/node_api/development/app.js
@@ -0,0 +1,267 @@
+const express = require('express')
+const app = express()
+
+const { MongoClient, ObjectId} = require('mongodb');
+require('dotenv/config');
+const client =  new MongoClient(process.env.DB_CONNECTION);
+
+const ServerError = function(msg) {
+    return {type: "server", msg: msg}
+}
+
+const months = [
+    "January", "February",
+    "March", "April", "May",
+    "June", "July", "August",
+    "September", "October",
+    "November", "December"
+    ];
+
+const approved = ['title', 'director', 'cast', 'country', 'release_year', 'rating', 'duration', 'listed_in', 'description']
+const necessities = ['title', 'director', 'duration', 'release_year']
+const typeChecks = function(req) {
+    return Object.keys(req).every(k => {
+        let t
+        switch(k){
+            case 'title':
+                t = typeof(req[k]) === 'string' && req[k].length > 0
+                console.log(`${k}: ${t}`)
+                return t
+                break
+            case 'rating':
+            case 'description':
+                t = typeof(req[k]) === 'string' && req[k].length >= 0
+                console.log(`${k}: ${t}`)
+                return t
+                break
+            case 'director':
+                t = req[k].length > 0 && req[k].every(e => typeof(e) === 'string' && e.length > 0)
+                console.log(`${k}: ${t}`)
+                return t
+                break
+            case 'cast':
+            case 'country':
+            case 'listed_in':
+                t = req[k].every(e => typeof(e) === 'string' && e.length > 0)
+                console.log(`${k}: ${t}`)
+                return t
+                break
+            case 'release_year':
+                let date = new Date()
+                t = typeof(req[k]) === 'number' && req[k] <= date.getFullYear()
+                console.log(`${k}: ${t}`)
+                return t
+                break
+            case 'duration':
+                t = typeof(req[k]) === 'number' && req[k] >= 0
+                console.log(`${k}: ${t}`)
+                return t
+                break
+            default:
+                console.log(`Default: ${false}`)
+                false
+        }
+    })
+}
+
+function suggestionChecks(filters) {
+    return Object.keys(filters).every(k => {
+        let t
+        switch(k) {
+            case 'title':
+            case 'description':
+                t = typeof(filters[k]) === 'string' && filters[k].length > 0 
+                break
+            case 'director':
+            case 'cast':
+            case 'listed_in':
+                t = filters[k].length > 0 && filters[k].every(e => typeof(e) === 'string' && e.length > 0)
+                break
+            default:
+                t = false 
+        }
+        return t
+    })
+} 
+
+function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
+
+function generateSearch(filters) {
+    let search = {type: 'Movie'}
+    search.$and = []
+    Object.keys(filters).forEach(k => {
+        switch(k) {
+            case 'title':
+            case 'description':
+                search[k] = {$regex: escapeRegExp(filters[k]), $options: 'i'}
+                break
+            case 'director':
+            case 'cast':
+            case 'listed_in':
+                potentials = []
+                filters[k].forEach(el => {
+                    obj = {}
+                    obj[k] = {$regex: escapeRegExp(el), $options: 'i'}
+                    potentials.push(obj)
+                })
+                search.$and.push({$or: potentials})
+                break
+            default:
+                //throw error
+                break
+        }
+    })
+    return search
+}
+
+const expandElement = function(key) {
+    switch(key) {
+        case 'rating':
+        case 'description': 
+        case 'country':
+        case 'listed_in':
+        case 'cast': 
+            return ""
+        default:
+            throw ServerError(`expandElement() key '${key}' not defined`)
+    }
+} 
+const prepareElement = function(key, req) {
+    switch(key) {
+        case 'title':
+        case 'description': 
+        case 'rating': 
+        case 'release_year':
+            return req[key]
+        case 'director':
+        case 'cast':
+        case 'country': 
+        case 'listed_in':
+            if (req[key].length === 0)
+                return ""
+            else
+                return req[key].reduce((acc, el, i) => {return acc += i === 0 ? el : `, ${el}`})
+        case 'duration':
+            return `${req[key]} min`
+        default:
+            throw ServerError(`prepareElement() key '${key}' not defined`)
+    }
+}
+
+app.use(express.json())
+
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+async function establishConnection() {
+    await client.connect()
+    const db = await client.db('NetflixTitles')
+    return await db.collection('netflix_titles')
+}
+
+
+function testCreate(req) {
+    const keys = Object.keys(req)
+    console.log(keys)
+    return keys.every(k => approved.includes(k)) && necessities.every(n => keys.includes(n)) && typeChecks(req)
+}
+
+app.post('/movie/create', async(req,res)=>{
+    const movie = req.body;
+    if (testCreate(movie)) {
+        try {
+            let newMovie = {}
+            approved.forEach(key => { newMovie[key] = movie[key] !== undefined ? prepareElement(key, movie) : expandElement(key)})
+            let date = new Date()
+            newMovie["type"] = "Movie"
+            newMovie["date_added"] = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+            console.log('Movie to create')
+            console.log(newMovie)
+            const collection = await establishConnection()
+            const f = await collection.insertOne(newMovie)
+            res.status(201).json({_id: f.insertedId})
+        } catch(err) {
+            console.log(err.msg)
+            res.sendStatus(500)
+        }
+    }
+    else 
+        res.sendStatus(400)
+})
+
+app.get('/movie', async(req,res)=> {
+    const id = req.query["_id"]
+    if (id === undefined || typeof(id) !== 'string' || id.length != 24) {
+        res.sendStatus(400)
+    } else {
+        const collection = await establishConnection()
+        const f = await collection.findOne({_id: new ObjectId(id)})
+        console.log(f)
+        if (!f)
+            res.sendStatus(204)
+        else
+            res.status(200).json(f)
+    }
+})
+
+app.delete('/movie', async(req,res)=> {
+    const id = req.body["_id"]
+    if (id === undefined || typeof(id) !== 'string') {
+        res.sendStatus(400)
+    } else {
+        const collection = await establishConnection()
+        await collection.deleteOne({_id: new ObjectId(id)})
+        res.sendStatus(200)
+    }
+})
+
+app.put('/movie/update', async(req,res)=> {
+    const id = req.body["_id"]
+    let updates = req.body
+    delete updates._id
+    const keys = Object.keys(updates)
+
+    if (id && keys.every(k => approved.includes(k)) && typeChecks(updates)) {
+        try {
+            movieUpdates = {}
+            keys.forEach(key => {
+                    movieUpdates[key] = prepareElement(key, updates)
+                })
+    
+            const collection = await establishConnection()
+            console.log(keys)
+            console.log(movieUpdates)
+            const f = await collection.findOneAndUpdate({_id:new ObjectId(id)}, {$set: movieUpdates})
+            console.log(f)
+            
+            res.sendStatus(200)
+        } catch(err) {
+            console.log(err.msg)
+            res.sendStatus(500)
+        }
+    } else 
+        res.sendStatus(400)
+})
+
+app.post('/movie/suggestions',async(req,res)=>{
+    const filters = req.body;
+    if(!suggestionChecks(filters)) {
+        await res.sendStatus(400)
+    }
+
+    const search = generateSearch(filters)
+    const collection = await establishConnection()
+    console.log('filters:')
+    console.log(filters);
+    console.log("search")
+    console.log(JSON.stringify(search, null, 4));
+    // console.log(search);
+    const result = await collection.find(search).toArray();
+
+    await res.json(result);
+})
+
+app.listen(3000)
